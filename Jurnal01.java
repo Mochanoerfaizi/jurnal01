779d75c5e72d405ac4126cb7689717ed7b884285
Mochamad Noer Faizi <mochnoerfaizi838@gmail.com>
2023-09-27 23:31:11 +0700

    menambahkan, NAMA, NIM, KELAS.

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+puiblic class Jurnal01{
+    public static void main(string[]args) {
+        System.out.println("Nama: Mochamad Noer Faizi Supriatna Putra")
+        System.out.println("NIM: 607062300109")
+        System.out.println("Kelas: D3IF-47-01")
+    }
+}
